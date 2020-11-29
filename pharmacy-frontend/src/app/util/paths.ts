48ec4a783a51378f5d821be40e73ce5bf0f4b628
majastamenic <stamenicmaja5@uns.ac.rs>
2020-11-29 05:17:19 +0100

    #Generated css file for actions and benefits.

--- pharmacy-frontend/src/app/util/paths.ts
+++ pharmacy-frontend/src/app/util/paths.ts
@@ -5,5 +5,5 @@ export const LOGIN_PATH = USER_PATH + '/login';
 
 export const PHARMACY_PATH = BACKEND_PATH + '/pharmacy';
 export const HOSPITAL_PATH = BACKEND_PATH + '/hospital';
-export const ACTION_PATH = BACKEND_PATH + '/rabbitmq';
+export const ACTION_PATH = BACKEND_PATH + '/rabbitmq/actions';
 export const REG_PATH = HOSPITAL_PATH + '//signup';
