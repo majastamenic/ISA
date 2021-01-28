45b9dfca89c4c13538c6f96c85a80e8e0ad57f53
majastamenic <stamenicmaja5@uns.ac.rs>
2021-01-28 19:16:05 +0100

    Add tests

--- pharmacy/src/test/java/com/isa/pharmacy/PharmacyApplicationTests.java
+++ pharmacy/src/test/java/com/isa/pharmacy/PharmacyApplicationTests.java
@@ -1,10 +1,15 @@
 package com.isa.pharmacy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootTest
 class PharmacyApplicationTests {
 
