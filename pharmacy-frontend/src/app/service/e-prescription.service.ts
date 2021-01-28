61e490c130f5fe14f7c0e15193d9cbca570e2a77
tasa1998 <tasa1998@uns.ac.rs>
2021-01-28 21:46:42 +0100

    [Merge] merge with s3

--- pharmacy-frontend/src/app/service/e-prescription.service.ts
+++ pharmacy-frontend/src/app/service/e-prescription.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ePrescriptionClass } from '../component/e-prescription/e-prescription.component';
 import { EPRESCRIPTION_PATH } from '../util/paths';
 
 @Injectable({
