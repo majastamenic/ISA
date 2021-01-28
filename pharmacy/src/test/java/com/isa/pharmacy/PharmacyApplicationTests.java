a259ae08d90de54497ab8fcd76fbffb504247e69
majastamenic <stamenicmaja5@uns.ac.rs>
2021-01-28 19:09:19 +0100

    Added false test

--- pharmacy/src/test/java/com/isa/pharmacy/PharmacyApplicationTests.java
+++ pharmacy/src/test/java/com/isa/pharmacy/PharmacyApplicationTests.java
@@ -3,11 +3,14 @@ package com.isa.pharmacy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class PharmacyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	    assertTrue(true);
 	}
 
 }
