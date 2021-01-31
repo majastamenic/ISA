4052ac66fd862e0e6299bad0762cba5b7ec0c27d
Gojko <gojko.novcic2@gmail.com>
2021-01-31 01:19:57 +0100

    [FeatureUpdate] Changed structure of email to hospital

--- pharmacy/src/main/resources/import.sql
+++ pharmacy/src/main/resources/import.sql
@@ -6,7 +6,7 @@ insert into app_user (address, city, country, email, name, password, phone, surn
 insert into pharmacy (id, address, name) values (1, 'Fejes Klare 34.', 'Jankovic');
 insert into pharmacy (id, address, name) values (2, 'Fejes Klare 34.', 'Benu');
 insert into pharmacy (id, address, name) values (3, 'Fejes Klare 34.', 'Zdravlje');
-insert into pharmacy (id, address, name) values (4, 'Fejes Klare 34.', 'Apoteka+');
+insert into pharmacy (id, address, name) values (4, 'Fejes Klare 34.', 'ApotekaPlus');
 insert into pharmacy (id, address, name) values (5, 'Fejes Klare 34.', 'Pharmacy');
 insert into pharmacy (id, address, name) values (6, 'Fejes Klare 34.', 'Nova');
 
@@ -23,12 +23,12 @@ INSERT INTO public.medicine_composition(medicine_id, composition)VALUES (2, 'tot
 INSERT INTO public.medicine_composition(medicine_id, composition)VALUES (2, 'ieie');
 
 -- Za apoteku sa id=1
-INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (1, 150, 2345, 1, 1);
-INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (2, 200, 2985, 2, 1);
-INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (3, 175, 1420, 3, 1);
+INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (1, 1.50, 2200, 1, 1);
+INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (2, 2.00, 2985, 2, 1);
+INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (3, 1.75, 1420, 3, 1);
 -- Za apoteku sa id=2
-INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (4, 144, 5, 1, 2);
-INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (5, 190, 8, 2, 2);
+INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (4, 1.44, 0, 1, 2);
+INSERT INTO public.medicine_pharmacy(id, price, quantity, medicine_id, pharmacy_id)VALUES (5, 1.90, 0, 2, 2);
 
 INSERT INTO public.medicine_medicine_pharmacy(medicine_id, medicine_pharmacy_id)VALUES (1, 1);
 INSERT INTO public.medicine_medicine_pharmacy(medicine_id, medicine_pharmacy_id)VALUES (2, 2);
