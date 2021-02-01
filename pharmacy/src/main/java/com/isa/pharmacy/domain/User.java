234f7fe307f26f8b933010a8fe306604af21eb18
majastamenic <stamenicmaja5@uns.ac.rs>
2021-02-01 02:46:36 +0100

    [Merge] Merged with develop

--- pharmacy/src/main/java/com/isa/pharmacy/domain/User.java
+++ pharmacy/src/main/java/com/isa/pharmacy/domain/User.java
@@ -1,11 +1,15 @@
 package com.isa.pharmacy.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
-import javax.persistence.*;
-
-@Inheritance
-@Entity(name = "app_user")
+@Entity
+@Table(name = "app_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
