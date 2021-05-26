79a9de51891d11fda3f32280a9894636fe7f1f91
tasa1998 <tasa1998@uns.ac.rs>
2021-05-27 00:13:49 +0200

    [Feature] confirmation and rejection vacations

--- pharmacy/src/main/java/com/isa/pharmacy/scheduling/service/interfaces/IVacationService.java
+++ pharmacy/src/main/java/com/isa/pharmacy/scheduling/service/interfaces/IVacationService.java
@@ -1,5 +1,6 @@
 package com.isa.pharmacy.scheduling.service.interfaces;
 
+import com.isa.pharmacy.controller.dto.VacationConfirmationDto;
 import com.isa.pharmacy.controller.dto.VacationScheduleDto;
 import com.isa.pharmacy.scheduling.domain.VacationSchedule;
 
@@ -18,4 +19,12 @@ public interface IVacationService {
      List<VacationScheduleDto> getVacationScheduleByPharmacist(String email);
 
      boolean compareDateWithVacations(List<VacationScheduleDto> pharmacistVacations, Date requiredStartDate, Date requiredEndDate);
+
+    List<VacationScheduleDto> getVacationScheduleDermatologists();
+
+     List<VacationScheduleDto> getVacationSchedulePharmacists(String email);
+
+    void confirmation(VacationConfirmationDto vacationSchedule);
+
+    void confirmationPharmacist(VacationConfirmationDto vacationSchedule);
 }
